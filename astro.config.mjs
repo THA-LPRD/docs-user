7d4dc61690638b63e7cbf5ec767bf9a9ca5d1f98
InfinitePain <emirhangoktas01@gmail.com>
2025-03-09 16:28:33 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,73 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: {
+            en: 'Docs with Tailwind and React',
+            de: 'Dokumentation mit Tailwind und React',
+        },
+        tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 2 },
+        editLink: {
+            baseUrl: 'https://github.com/THA-LPRD/docs-user/edit/main/',
+        },
+        social: {
+            github: 'https://github.com/THA-LPRD',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                translations: {
+                    de: 'Anleitungen',
+                },
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    {
+                        label: 'Example Guide',
+                        slug: 'guides/example',
+                        translations: {
+                            de: 'Beispiel Anleitung',
+                        },
+                    },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+		],
+        customCss: [
+            '@fontsource-variable/inter-tight',
+            '@fontsource-variable/roboto-mono',
+            './src/styles/global.css',
+        ],
+        lastUpdated: true,
+        pagination: false,
+        defaultLocale: 'root',
+        locales: {
+            // English docs in `src/content/docs/`
+            root: {
+                label: 'English',
+                lang: 'en',
+            },
+            // German docs in `src/content/docs/de/`
+            de: {
+                label: 'Deutsch',
+                lang: 'de',
+            },
+        },
+        }),
+        react()
+    ],
+    trailingSlash: 'never',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    site: 'https://tha-lprd.github.io',
+    base: 'docs-user',
+});
---
+++ src/content/docs/de/guides/example.md
@@ -0,0 +1,11 @@
+---
+title: Beispielanleitung
+description: Eine Anleitung in meiner neuen Starlight-Dokumentationsseite.
+---
+
+Anleitungen führen einen Benutzer durch eine bestimmte Aufgabe, die sie erledigen möchten, oft mit einer Reihe von Schritten.
+Eine gute Anleitung zu schreiben erfordert, dass du darüber nachdenkst, was deine Benutzer versuchen zu tun.
+
+## Weiterführende Lektüre
+
+- Lies [über Anleitungen](https://diataxis.fr/how-to-guides/) im Diátaxis-Framework
